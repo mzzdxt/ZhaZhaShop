875438cfa48f8a86a2c5e9189fbd63ed97c23691
coderwjq <coderwjq@126.com>
2017-05-25 17:07:57 +0800

    完善FragmentMovieMain顶部指示器动画

---
+++ shop/src/main/java/com/coderwjq/shop/module/movie/movie_find/FragmentMovieFind.java
@@ -0,0 +1,31 @@
+package com.coderwjq.shop.module.movie.movie_find;
+
+import com.coderwjq.shop.R;
+import com.coderwjq.shop.base.BaseFragment;
+
+/**
+ * @Created by coderwjq on 2017/5/25 16:01.
+ * @Desc
+ */
+
+public class FragmentMovieFind extends BaseFragment {
+
+    public static FragmentMovieFind getInstance() {
+        return new FragmentMovieFind();
+    }
+
+    @Override
+    protected int getLayoutId() {
+        return R.layout.fragment_find_movie;
+    }
+
+    @Override
+    protected void initData() {
+
+    }
+
+    @Override
+    protected void initListener() {
+
+    }
+}
