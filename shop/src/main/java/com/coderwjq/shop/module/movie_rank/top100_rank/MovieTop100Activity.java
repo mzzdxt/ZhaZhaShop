0302ca6898cb74bd972589c34cb9ddae250d01a7
coderwjq <coderwjq@126.com>
2017-09-05 09:17:01 +0800

    添加top100电影列表

---
+++ shop/src/main/java/com/coderwjq/shop/module/movie_rank/top100_rank/MovieTop100Activity.java
@@ -0,0 +1,15 @@
+package com.coderwjq.shop.module.movie_rank.top100_rank;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.coderwjq.shop.R;
+
+public class MovieTop100Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_movie_top100);
+    }
+}
